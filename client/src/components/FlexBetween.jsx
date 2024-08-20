65a411d4349080f5549ce3a5dd7016ed71d29728
mwguo15 <mwguo@andrew.cmu.edu>
2024-08-20 17:36:42 -0400

    Finished project

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+/* reused css properties */
+const FlexBetween = styled(Box)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+})
+
+export default FlexBetween;
---
+++ client/src/scenes/loginPage/index.jsx
@@ -0,0 +1,39 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        // Sociopedia icon at top
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
+
+      {/* Box for containing form (email and password input) */}
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Sociopedia, the Social Media for Sociopaths!
+        </Typography>
+        {/* Form then contains all the user clickable/input things */}
+        <Form /> 
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
